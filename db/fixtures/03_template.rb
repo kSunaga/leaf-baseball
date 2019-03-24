727346c182ec3bcab42bf9aca59c48523b640c88
kSunaga <jokers4140@gmail.com>
2019-03-24 22:32:13 +0900

    seedを追加

---
+++ db/fixtures/03_template.rb
@@ -0,0 +1,53 @@
+Template.seed do |s|
+  s.id = 1
+  s.title = 'template1'
+  s.name = 'takeshi'
+  s.email = 'admin@admin.com'
+  s.content = 'contentnet'
+  s.user = User.find(1)
+end
+
+Template.seed do |s|
+  s.id = 2
+  s.title = 'template1'
+  s.name = 'takeshi'
+  s.email = 'admin@admin.com'
+  s.content = 'contentnet'
+  s.user = User.find(1)
+end
+
+Template.seed do |s|
+  s.id = 3
+  s.title = 'template1'
+  s.name = 'nobita'
+  s.email = 'admin@admin.com'
+  s.content = 'contentnet'
+  s.user = User.find(2)
+end
+
+Template.seed do |s|
+  s.id = 4
+  s.title = 'template1'
+  s.name = 'nobita'
+  s.email = 'admin@admin.com'
+  s.content = 'contentnet'
+  s.user = User.find(2)
+end
+
+Template.seed do |s|
+  s.id = 5
+  s.title = 'template1'
+  s.name = 'suneo'
+  s.email = 'admin@admin.com'
+  s.content = 'contentnet'
+  s.user = User.find(3)
+end
+
+Template.seed do |s|
+  s.id =6
+  s.title = 'template1'
+  s.name = 'suneo'
+  s.email = 'admin@admin.com'
+  s.content = 'contentnet'
+  s.user = User.find(3)
+end
