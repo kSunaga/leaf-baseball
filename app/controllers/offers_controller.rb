142ed441c0d8bc7af022c7ad3e88a8e9e1df9221
kSunaga <jokers4140@gmail.com>
2019-03-25 12:59:59 +0900

    テンプレートをoffer登録画面に適応

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,8 +1,15 @@
 class OffersController < ApplicationController
 
+  def new
+    @offer = Offer.new
+    if params[:template]
+      @template = Template.find(params[:template][:id])
+    end
+  end
+
   def create
     offer = Offer.new(set_params)
-    offer.board_id = params[:format]
+    offer.board_id = params[:board_id]
 
     if offer.save!
       flash[:message] = "申し込みに成功しました。"
