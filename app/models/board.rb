5de31d65b25498734b1bc55c0454f0e2fd1123a3
kSunaga <jokers4140@gmail.com>
2019-03-25 21:23:04 +0900

    バリデーションを追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,12 @@ class Board < ApplicationRecord
   has_many :offers
   belongs_to :user, optional: true
 
+  validates :name, presence: true
+  validates :place, presence: true
+  validates :game_date, presence: true
+  validates :level, presence:  true
+  validates :content, presence: true
+
   def self.is_password_valid(password, confirm_password)
     password == confirm_password
   end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,8 @@
 class Template < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :content, presence: true
 end
