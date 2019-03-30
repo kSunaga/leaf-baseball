5b0ae9848f6103b9c14948b17496ba3ebad4acb9
kSunaga <jokers4140@gmail.com>
2019-03-31 04:03:45 +0900

    i18n適用

--- app/decorators/board_decorator.rb
+++ app/decorators/board_decorator.rb
@@ -1,6 +1,10 @@
 module BoardDecorator
 
-  def format_date
-    game_date.strftime('%Y年%m月%d日')
+  def format_start_date
+    game_start_date.strftime('%Y年%m月%d日 %H時%m分')
+  end
+
+  def format_end_date
+    game_end_date.strftime('%Y年%m月%d日 %H時%m分')
   end
 end
